2db995aa989a308061aebb2edaf156aa36c15f20
rottaj <jackrotta12@gmail.com>
2021-12-07 18:27:25 -0500

    updated

--- python/fixed_xor.py
+++ python/fixed_xor.py
@@ -1,16 +1,12 @@
 hex_string = "1c0111001f010100061a024b53535009181c"
 hex_string2 = "686974207468652062756c6c277320657965"
 
-x = bin(int(hex_string, 16))[2:]
-x2 = bin(int(hex_string, 16))[2:]
+x = bin(int(hex_string, 16))[2:] # remove trailing c0
+x2 = bin(int(hex_string2, 16))[2:]
+trailing = len(x) if len(x) > len(x2) else len(x2)
+x = x.zfill(trailing)
+x2 = x2.zfill(trailing)
 print(x, x2, "\n", len(x), len(x2))
-
-fill_zeros = len(x) if len(x) > len(x2) else len(x2)
-x = x.zfill(fill_zeros)
-x2 = x2.zfill(fill_zeros)
-
-new = [int(bit1) ^ int(bit2) for bit1, bit2 in zip(list(x), list(x2))]
-print("\n", new)
-res = "".join([str(i) for i in new])
-print("\n\n", res)
+c = "".join([str(int(i1) ^ int(i2)) for i1, i2 in zip(x, x2)])
+print(hex(int(c, 2))[2:])
 
