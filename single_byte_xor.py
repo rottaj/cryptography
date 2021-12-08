bc79240458f9bbec8c2b86371f854f17b82cd159
rottaj <jackrotta12@gmail.com>
2021-12-07 20:48:57 -0500

    updated

--- single_byte_xor.py
+++ single_byte_xor.py
@@ -1,8 +1,22 @@
-hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+import sys
 
-hex_vals = [int(i, 16) for i in hex_string]
-print(hex_vals, "\n")
-xor_vals = [i ^ 3 for i in hex_vals]
-print(xor_vals)
-ret = ''.join([hex(i) for i in hex_vals])
-print("\n", ret)
+def find_byte_xor(key):
+  hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+  key_hash = str(key) * len(hex_string)
+  print(len(hex_string), len(key_hash))
+  x1 = bin(int(hex_string, 16))[2:] #hexidecimal (base 16) to binary
+  x2 = bin(int(key_hash, 10))[2:] #decimal (base 10) to binary
+  x2 = x2.zfill(len(x1))
+  print(x1, "\n\n", x2, "\n")
+  print(len(x1), len(x2))
+  c = "".join([str(int(i1) ^ int(i2)) for i1, i2 in zip(x1, x2)])
+  ret = str(hex(int(c, 2))[2:])
+  return ret
+
+if __name__ == "__main__":
+  print("ENTER KEY: ")
+  key = input()
+  ret = find_byte_xor(key)
+  sys.stdout.write(ret)
+
+
