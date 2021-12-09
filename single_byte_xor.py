4da362e1c7857ec1f6386b2e198bac405dbc2434
rottaj <jackrotta12@gmail.com>
2021-12-09 18:56:21 -0500

    updated

--- single_byte_xor.py
+++ single_byte_xor.py
@@ -2,21 +2,19 @@ import sys
 
 def find_byte_xor(key):
   hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-  key_hash = str(key) * len(hex_string)
-  print(len(hex_string), len(key_hash))
-  x1 = bin(int(hex_string, 16))[2:] #hexidecimal (base 16) to binary
-  x2 = bin(int(key_hash, 10))[2:] #decimal (base 10) to binary
-  x2 = x2.zfill(len(x1))
-  print(x1, "\n\n", x2, "\n")
-  print(len(x1), len(x2))
-  c = "".join([str(int(i1) ^ int(i2)) for i1, i2 in zip(x1, x2)])
-  ret = str(hex(int(c, 2))[2:])
+  split = [hex_string[i:i+2] for i in range(0, len(hex_string), 2)]
+  print(split)
+  print(int(split[0], 16) ^ int(key, 36))
+  testing = "".join([bin(int(i, 16) ^ int(key, 36)) for i in split])
+  print(hex(int(testing, 2)[2:]))
+  '''
+  decimals = "".join([bin(int(i, 16) ^ int(key, 16))[2:] for i in hex_string])
+  ret = hex(int(decimals, 2))[2:]
   return ret
-
+  '''
 if __name__ == "__main__":
-  print("ENTER KEY: ")
-  key = input()
-  ret = find_byte_xor(key)
-  sys.stdout.write(ret)
+  key = sys.argv[1]
+  find_byte_xor(key)
+  #sys.stdout.write(ret)
 
 
