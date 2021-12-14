c4c4f39bf70fbe32eaff0455d51385a523822074
rottaj <jackrotta12@gmail.com>
2021-12-14 00:36:33 -0500

    updated

--- python/aes_ecb_mode.py
+++ python/aes_ecb_mode.py
@@ -1,9 +1,15 @@
+from Crypto.Cipher import AES
+
 def aes_in_ecb(c, k):
-  print(c, k)
+  cipher = AES.new(k.encode("utf8"), AES.MODE_ECB)
+  plainText = cipher.decrypt(c.encode("utf8"))
+  print(plainText)
+
 
 if __name__ == "__main__":
 
   k = "YELLOW SUBMARINE"
-  with open("../datfile/aes_data.dat") as f:
+  with open("../datfile/aes_base64.dat") as f:
     data = f.readlines()
-    aes_in_ecb(data, k)
+    print(''.join(data)) 
+    aes_in_ecb(''.join(data), k)
