5b12eca9b40b4363ca4a225a3f5a433452162352
rottaj <jackrotta12@gmail.com>
2021-12-14 19:36:12 -0500

    updated pcks padding

--- python/pcks7_padding.py
+++ python/pcks7_padding.py
@@ -1,25 +1,22 @@
 
-def pkcs_padding(plainText):
-  textArr = list(plainText)
-  if len(textArr) % 4 == 0:
-    i = 0
-    while i <= len(textArr) / 4:
-      textArr.append("\x04")
-      i +=1
-  elif len(textArr) % 2 == 0:
-    i = 0
-    while i <= len(textArr) / 2:
-      textArr.append("\x02")
-      i +=1
-  elif len(textArr) % 3 == 0:
-    i = 0
-    while i <= len(textArr) / 3:
-      textArr.append("\x03")
-      i +=1
+def pad(byteList, padding, b):
+  if len(byteList) == padding:
+    return byteList
+  else:
+    byteList.append(b)
+    return pad(byteList, padding, b)
 
-  return ''.join(textArr)
+
+def pkcs_padding(plainText, padding):
+  l = [bytes(i.encode("utf8")) for i in list(plainText)]
+  if (padding - len(l)) % 4 == 0:
+    byteList = pad(l, padding, b"\04")
+  elif (padding - len(l)) % 2 == 0:
+    byteList = pad(l, padding, b"\02")
+
+  return b''.join(byteList)
 
 if __name__ == "__main__":
   plainText = "YELLOW SUBMARINE"
-  cipherText = pkcs_padding(plainText)
-  print(cipherText, len(cipherText))
+  cipherText = pkcs_padding(plainText, 22)
+  print(cipherText)
